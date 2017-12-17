3d6931bae2c77139580f011c16060ec5ed2c7542
zhengyuqi <zhengyuqi@baidu.com>
2017-12-17 23:57:01 +0800

    update to Perfect 3.x

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "Pjango",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
     ]
 )
