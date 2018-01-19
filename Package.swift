a551e770d5f8fd2339026834c1f01a6afdedd7ef
zhengyuqi <zhengyuqi@baidu.com>
2018-01-19 10:02:17 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,25 +1,12 @@
 import PackageDescription
 
-#if os(OSX)
 let package = Package(
     name: "Pjango",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
         ]
 )
-#else
-let package = Package(
-    name: "Pjango",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
-        ]
-)
-#endif
-
 
